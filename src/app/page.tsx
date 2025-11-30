ade70ae775979930e64568e4e6896d5eaf8a6251
James Galvis <galvisrodriguez618@gmail.com>
2025-11-29 21:31:07 -0500

    feat 02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import { Button } from "@/components/ui/button";
+import { Button, Checkbox, Input, Progress, Textarea } from "@/components/ui"
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">Button</Button>
+        </div>
+        <div>
+          <Input placeholder="I'm an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="I'm a textarea" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   )
 }
