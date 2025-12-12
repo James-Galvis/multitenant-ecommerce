9226b311d179b36279ed05129746e2c014476ee4
James Galvis <galvisrodriguez618@gmail.com>
2025-12-12 18:06:28 -0500

    feat 06: categories finalization

--- src/components/search-filters/search-input.tsx
+++ src/components/search-filters/search-input.tsx
@@ -1,13 +1,28 @@
-import { SearchIcon } from "lucide-react";
+"Use client";
+
+import { useState } from "react";
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+
+import { CustomCategory } from "@/types";
+import { Button } from "@/components/ui";
 import { Input } from "@/components/ui/input";
+import { CategoriesSidebar } from "@/components/search-filters/categories-sidebar";
 
 interface Props {
   disabled?: boolean;
+  data: CustomCategory[];
 }
 
-export function SearchInput({ disabled }: Props) {
+export function SearchInput({ disabled, data }: Props) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex items-center gap-2 w-full">
+      <CategoriesSidebar
+        data={data}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
       <div className="relative w-full">
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
         <Input
@@ -16,6 +31,13 @@ export function SearchInput({ disabled }: Props) {
           disabled={disabled}
         />
       </div>
+      <Button
+        variant="elevated"
+        className="size-12 shrink-0 flex lg:hidden"
+        onClick={() => setIsSidebarOpen(true)}
+      >
+        <ListFilterIcon />
+      </Button>
     </div>
   );
 }
